df4cd4c504d2f9a7ffa296e2865697f734906c67
jayr0flmc <jaymc@arcor.de>
2015-01-03 17:35:15 +0100

    Actually version added.

---
+++ @chronicles/mod.cpp
@@ -0,0 +1,6 @@
+dir			= "@chronicles";
+name		= "Chronicles Survival MoD 0.0.0.1";
+actionName	= "Website";
+picture		= "mod.paa";
+action		= "http://chronicles.elbental-productions.de";
+overview	= "Coming soon...";
